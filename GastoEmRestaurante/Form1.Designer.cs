80bace76ba0a59f159cc82604aef41cdfe1b6df0
Arlei <arleicarbonato@gmail.com>
2022-12-20 20:59:21 -0300

    Criar Projeto

---
+++ GastoEmRestaurante/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace GastoEmRestaurante
+{
+    partial class frmGastoRestaurante
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblDespesa = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtDespesa = new System.Windows.Forms.TextBox();
+            this.txtTotalConta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(292, 27);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblDespesa
+            // 
+            this.lblDespesa.AutoSize = true;
+            this.lblDespesa.Location = new System.Drawing.Point(35, 27);
+            this.lblDespesa.Name = "lblDespesa";
+            this.lblDespesa.Size = new System.Drawing.Size(49, 13);
+            this.lblDespesa.TabIndex = 1;
+            this.lblDespesa.Text = "Despesa";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Blue;
+            this.lblTotal.Location = new System.Drawing.Point(38, 71);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(98, 13);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "Total com 10% :";
+            // 
+            // txtDespesa
+            // 
+            this.txtDespesa.Location = new System.Drawing.Point(155, 27);
+            this.txtDespesa.Name = "txtDespesa";
+            this.txtDespesa.Size = new System.Drawing.Size(100, 20);
+            this.txtDespesa.TabIndex = 3;
+            // 
+            // txtTotalConta
+            // 
+            this.txtTotalConta.BackColor = System.Drawing.Color.Yellow;
+            this.txtTotalConta.Enabled = false;
+            this.txtTotalConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalConta.Location = new System.Drawing.Point(155, 64);
+            this.txtTotalConta.Name = "txtTotalConta";
+            this.txtTotalConta.Size = new System.Drawing.Size(212, 20);
+            this.txtTotalConta.TabIndex = 4;
+            this.txtTotalConta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmGastoRestaurante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 120);
+            this.Controls.Add(this.txtTotalConta);
+            this.Controls.Add(this.txtDespesa);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDespesa);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "frmGastoRestaurante";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gasto em restaurante";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblDespesa;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtDespesa;
+        private System.Windows.Forms.TextBox txtTotalConta;
+    }
+}
+
---
+++ GastoEmRestaurante/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GastoEmRestaurante
+{
+    public partial class frmGastoRestaurante : Form
+    {
+        public frmGastoRestaurante()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            txtTotalConta.Text = (Convert.ToDouble(txtDespesa.Text) * 1.10).ToString("N");
+        }
+    }
+}
